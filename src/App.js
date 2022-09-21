d9cf6751fef2cb7009f4b1201289ce03d0c97b87
Kamal Mahmudi <kamalmahmudi@gmail.com>
2022-09-21 23:53:19 +0700

    added other stock-info components

--- src/App.js
+++ src/App.js
@@ -12,6 +12,27 @@ function App() {
             <div className='stat positive'>+0.12%</div>
           </div>
         </div>
+        <div className='stock-info'>
+          <div className='name'>GOOGL</div>
+          <div className='price-info'>
+            <div className='price'>$101.47</div>
+            <div className='stat positive'>+0.29%</div>
+          </div>
+        </div>
+        <div className='stock-info'>
+          <div className='name'>MSFT</div>
+          <div className='price-info'>
+            <div className='price'>$245.06</div>
+            <div className='stat positive'>+1.08%</div>
+          </div>
+        </div>
+        <div className='stock-info'>
+          <div className='name'>ORCL</div>
+          <div className='price-info'>
+            <div className='price'>$68.38</div>
+            <div className='stat positive'>+0.46%</div>
+          </div>
+        </div>
       </div>
     </div>
   );
